a2f49516043dad0aaa8af5ff9919e3f04da57c56
Daniel Mills <danielmillst@gmail.com>
2016-10-14 05:50:18 -0400

    Sync start fix

--- src/main/java/org/cyberpwn/effex/Effex.java
+++ src/main/java/org/cyberpwn/effex/Effex.java
@@ -1,7 +1,9 @@
 package org.cyberpwn.effex;
 
 import org.phantomapi.construct.Ghost;
+import org.phantomapi.core.SyncStart;
 
+@SyncStart
 public class Effex extends Ghost
 {
 	private EffexController effexController;
@@ -9,23 +11,23 @@ public class Effex extends Ghost
 	@Override
 	public void preStart()
 	{
-		this.effexController = new EffexController(this);
+		effexController = new EffexController(this);
 		
 		register(effexController);
 	}
-
+	
 	@Override
 	public void onStart()
 	{
 		
 	}
-
+	
 	@Override
 	public void onStop()
 	{
 		
 	}
-
+	
 	@Override
 	public void postStop()
 	{
